68cc2f211fafff35dbef67b495a2e4b7fd8432cd
Yavuz KUTUK <yavuz@kutuk.fr>
2024-03-13 15:32:49 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+// `$users` contenant une série de prénoms (Simon, Arthur, Yohan),
+// * *"Soyez le premier à aimer cette publication"*,
+// * *"Simon aime cette publication"*,
+// * *"Simon et Arthur aiment cette publication"*,
+// * *"Simon et X autres personnes ont aimé cette publication."*,
+
+/////////////////////////zone d'attente
+// uncomment lines for testing
+$users = [];
+// $users = ['Simon'];
+// $users = ['Yohan'];
+// $users = ['Pierre', 'Marie'];
+// $users = ['Paul', 'Arthur'];
+// $users = ['Arthur', 'Paul', 'Julien', 'Simon', 'Valentin', 'Laurent'];
+// $users = ['Virginie', 'Paul', 'Julien', 'Simon'];
+
+//write you're code below
